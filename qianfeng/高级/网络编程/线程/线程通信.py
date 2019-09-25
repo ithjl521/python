01fc2a96e25031bb1c626803a2b1ece000e4b5b8
unknown <it_hjl@163.com>
2019-09-25 20:50:02 +0800

    协程“ ‘

---
+++ qianfeng/高级/网络编程/线程/线程通信.py
@@ -0,0 +1,28 @@
+import threading
+import time
+
+def func():
+	# 事件对象
+	event = threading.Event()
+
+	def run():
+		for i in range(5):
+			# 阻塞，等待事件触发
+			event.wait()
+
+			# 重置
+			event.clear()
+			print('hjl is a good man!%d'%i)
+
+	t = threading.Thread(target=run).start()
+
+	return event
+
+e = func()
+
+# 触发事件
+for i in range(5):
+	time.sleep(2)
+	e.set()
+
+
