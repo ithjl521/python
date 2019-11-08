f4fe50799501c483cb64445fd05ee0f30f56576c
ithjl521 <it_hjl@163.com>
2019-11-08 16:59:35 +0800

    over

--- qianfeng/高级/Restful/HelloRestfull/App/urls.py
+++ qianfeng/高级/Restful/HelloRestfull/App/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from App import views
 
 urlpatterns = [
-    path('index/', views.index),
+    path('hello/', views.HelloView.as_view(), name='hello'),
+    path('template/', views.HelloTemplateView.as_view(), name='template'),
+    path('listview/', views.HelloListView.as_view(), name='listview'),
+    path('single/<int:pk>/', views.HelloDetailView.as_view(), name='single'),
 
 ]
--- qianfeng/高级/Restful/HelloRestfull/App/views.py
+++ qianfeng/高级/Restful/HelloRestfull/App/views.py
@@ -1,8 +1,33 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
+from App.models import Book
 
 
-def index(request):
-    return HttpResponse('hello')
+class HelloView(View):
+
+    def get(self, request):
+        return render(request, 'hello.html')
+
+    def post(self, request):
+        data = {
+            'status': 200,
+        }
+
+        return JsonResponse(data=data)
+
+
+class HelloTemplateView(TemplateView):
+    template_name = 'hello.html'
+
+
+class HelloListView(ListView):
+    template_name = 'book_list.html'
+    model = Book
+
+
+class HelloDetailView(DetailView):
+    template_name = 'book.html'
+    model = Book
